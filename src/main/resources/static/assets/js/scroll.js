56b2d18afedfbfb78587f9f065de98bef87a91be
CODERDRIVER <1187697635@qq.com>
2018-10-20 23:12:16 +0800

    debug

--- src/main/resources/static/assets/js/scroll.js
+++ src/main/resources/static/assets/js/scroll.js
@@ -1,33 +1,6 @@
 var str ="";
 $(document).ready(function(){
 	$.ajax({
-<<<<<<< HEAD
-		   type:'GET',
-		   dataType:'json',
-		   url:'/announcements',
-		   contentType:'application/json;charset=UTF-8',
-		   async: false,
-
-		   success:function(data){//返回结果
-			   console.log(data);
-				 announcements = data.data;
-			   document.getElementById("scrollBox").innerHTML = "";
-			   for(var i=0; i<announcements.length;i++){
-				   var j = i+1;
-				  str+= "<li><a href='#'>"+j+":"+announcements[i].title+"</a></li>"
-				  // message.push(new announcement(announcements[i].id,announcements[i].title,announcements[i].content));
-				   }
-				   document.getElementById("scrollBox").innerHTML="<ul>"+str+"</ul>";
-			   console.log(document.getElementById("scrollBox").innerHTML);
-		   }
-
-
-	   });
-
-
-});
-
-=======
 			   type:'GET',
 			   dataType:'json',
 			   url:'/announcements',
@@ -84,6 +57,5 @@ $(document).ready(function(){
 		 $("#scrollBox").mouseover(function(){clearInterval(timer)});
 		 //$("#scrollBox").mouseout(function(){timer=setInterval('scrollUp()',speed)});
 })
->>>>>>> ad8278567cb965c9ed87a1cbe43d552e7df0f271
 
 	
