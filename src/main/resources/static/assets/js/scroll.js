7dee1c70f643d6837eec8feef909ae8b5d28d901
CODERDRIVER <1187697635@qq.com>
2018-10-20 20:43:08 +0800

    update announcement

--- src/main/resources/static/assets/js/scroll.js
+++ src/main/resources/static/assets/js/scroll.js
@@ -10,13 +10,15 @@ $(document).ready(function(){
 			   success:function(data){//返回结果
 				   console.log(data);
 					 announcements = data.data;
+                   document.getElementById("scrollBox").innerHTML = "";
 				   for(var i=0; i<announcements.length;i++){
 					   var j = i+1;
 					  str+= "<li><a href='#'>"+j+":"+announcements[i].title+"</a></li>"
 					  // message.push(new announcement(announcements[i].id,announcements[i].title,announcements[i].content));
 					   }
 					   document.getElementById("scrollBox").innerHTML="<ul>"+str+"</ul>";
-			   }
+                   console.log(document.getElementById("scrollBox").innerHTML);
+               }
 
 			   
 		   });	
